ba04b5ffb4bef5596777d4634aaac399d0c66586
antoniowellington542 <antoniowellington542@gmail.com>
2022-07-02 15:43:12 -0300

    CreateUser finish

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,20 @@
 import { v4 as uuidV4 } from "uuid";
 
+// Modelo do usuario
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  update_at: Date;
+
+  constructor() {
+    // Se não existir Id, ele é criado.
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { User };
